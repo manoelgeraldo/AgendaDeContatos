699ce455e7ecdcb5fadaa7756b88fdabda97d8bf
manoelgeraldo <manoelgeraldo.com@gmail.com>
2021-03-09 01:48:36 -0300

    commit initial

---
+++ AgendaTelefonica/FormMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace AgendaTelefonica
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.dgvContatos = new System.Windows.Forms.DataGridView();
+            this.lbl_Id = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefone";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(147, 82);
+            this.txtTelefone.MaxLength = 13;
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(278, 30);
+            this.txtTelefone.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(147, 35);
+            this.txtNome.MaxLength = 20;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(278, 30);
+            this.txtNome.TabIndex = 3;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalvar.Location = new System.Drawing.Point(17, 149);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(132, 55);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditar.Location = new System.Drawing.Point(155, 149);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(132, 55);
+            this.btnEditar.TabIndex = 5;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExcluir.Location = new System.Drawing.Point(293, 149);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(132, 55);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // dgvContatos
+            // 
+            this.dgvContatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContatos.Location = new System.Drawing.Point(17, 228);
+            this.dgvContatos.MultiSelect = false;
+            this.dgvContatos.Name = "dgvContatos";
+            this.dgvContatos.ReadOnly = true;
+            this.dgvContatos.RowHeadersVisible = false;
+            this.dgvContatos.RowHeadersWidth = 51;
+            this.dgvContatos.RowTemplate.Height = 24;
+            this.dgvContatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvContatos.Size = new System.Drawing.Size(408, 150);
+            this.dgvContatos.TabIndex = 7;
+            this.dgvContatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvContatos_CellClick);
+            // 
+            // lbl_Id
+            // 
+            this.lbl_Id.AutoSize = true;
+            this.lbl_Id.Location = new System.Drawing.Point(52, 9);
+            this.lbl_Id.Name = "lbl_Id";
+            this.lbl_Id.Size = new System.Drawing.Size(76, 17);
+            this.lbl_Id.TabIndex = 8;
+            this.lbl_Id.Text = "Id_Contato";
+            this.lbl_Id.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(441, 402);
+            this.Controls.Add(this.lbl_Id);
+            this.Controls.Add(this.dgvContatos);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contatos";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView dgvContatos;
+        private System.Windows.Forms.Label lbl_Id;
+    }
+}
+
---
+++ AgendaTelefonica/FormMain.cs
@@ -0,0 +1,158 @@
+﻿using AgendaTelefonica.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgendaTelefonica
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Display();
+        }
+        /// 
+        /// MOSTRA OS DADOS DO BANCO NO DATAGRIDVIEW
+        /// 
+        public void Display()
+        {
+            using (agendaTelefonicaEntities _entity = new agendaTelefonicaEntities())
+            {
+                List<Contatos> listaDeContatos = new List<Contatos>();
+                listaDeContatos = _entity.tb_contatos.Select(x => new Contatos { Id = x.id, Nome = x.nome, Telefone = x.telefone }).ToList();
+                dgvContatos.DataSource = listaDeContatos;
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            tb_contatos contato = new tb_contatos();
+            contato.nome = txtNome.Text;
+            contato.telefone = txtTelefone.Text;
+            bool result = SalvarContato(contato);
+            ShowStatus(result, "Novo");
+        }
+        public bool SalvarContato(tb_contatos contato)
+        {
+            bool result = false;
+            using (agendaTelefonicaEntities _entity = new agendaTelefonicaEntities())
+            {
+                _entity.tb_contatos.Add(contato);
+                _entity.SaveChanges();
+                result = true;
+            }
+            return result;
+        }
+        ///
+        /// Validar o status da operação e mostrar as mensagens ao usuário
+        ///
+        public void ShowStatus(bool result, string Action)
+        {
+            if (result)
+            {
+                if (Action.ToUpper() == "NOVO")
+                {
+                    MessageBox.Show("Contato Salvo!", "Novo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (Action.ToUpper() == "EDITAR")
+                {
+                    MessageBox.Show("Contato Editado!", "Editar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Contato Excluido!", "Excluir", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Houve algum erro! Por favor tente novamente!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            LimparCampos();
+            Display();
+        }
+        /// 
+        /// Limpar os campos após a operação de Inserir ou Atualizar ou Excluir
+        ///
+        public void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtTelefone.Text = "";
+        }
+        /// <summary>
+        /// Valores da linha selecionada
+        /// </summary>
+        private void dgvContatos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvContatos.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dgvContatos.SelectedRows)
+                {
+                    lbl_Id.Text = row.Cells[0].Value.ToString();
+                    txtNome.Text = row.Cells[1].Value.ToString();
+                    txtTelefone.Text = row.Cells[2].Value.ToString();
+                }
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            tb_contatos contato = SetValues(Convert.ToInt32(lbl_Id.Text), txtNome.Text, txtTelefone.Text);
+            bool result = EditarContato(contato);
+            ShowStatus(result, "EDITAR");
+        }
+        public bool EditarContato(tb_contatos contato)
+        {
+            bool result = false;
+            using (agendaTelefonicaEntities _entity = new agendaTelefonicaEntities())
+            {
+                tb_contatos _contato = _entity.tb_contatos.Where(x => x.id == contato.id).Select(x => x).FirstOrDefault();
+                _contato.id = contato.id;
+                _contato.nome = contato.nome;
+                _contato.telefone = contato.telefone;
+                _entity.SaveChanges();
+                result = true;
+            }
+            return result;
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            tb_contatos contato = SetValues(Convert.ToInt32(lbl_Id.Text), txtNome.Text, txtTelefone.Text);
+            bool result = ExcluirContato(contato);
+            ShowStatus(result, "EXCLUIR");
+        }
+        public bool ExcluirContato(tb_contatos contato)  
+        {
+            bool result = false;
+            using (agendaTelefonicaEntities _entity = new agendaTelefonicaEntities())
+            {
+                tb_contatos _contato = _entity.tb_contatos.Where(x => x.id == contato.id).Select(x => x).FirstOrDefault();
+                _entity.tb_contatos.Remove(_contato);
+                _entity.SaveChanges();
+                result = true;
+            }
+            return result;
+        }
+        public tb_contatos SetValues(int id, string nome, string telefone)
+        {
+            tb_contatos contato = new tb_contatos
+            {
+                id = id,
+                nome = nome,
+                telefone = telefone
+            };
+            return contato;
+        }
+    }
+}
